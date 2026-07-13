86d9092e92bde2ddd4bdd72f66179a52fe38b7de
tkasierski <thomaskasierski@gmail.com>
2026-07-13 08:25:26 -0600

    Expose modular public API

--- src/covariance_matrix/__init__.py
+++ src/covariance_matrix/__init__.py
@@ -1,15 +1,3 @@
-"""Covariance matrix analysis package."""
+from .core import build_covariance_excel, parse_tickers
 
-from covariance_matrix.core import (
-    build_covariance_excel,
-    calculate_downside_risk_metrics,
-    calculate_drawdown_metrics,
-    parse_tickers,
-)
-
-__all__ = [
-    "build_covariance_excel",
-    "calculate_downside_risk_metrics",
-    "calculate_drawdown_metrics",
-    "parse_tickers",
-]
+__all__ = ["build_covariance_excel", "parse_tickers"]
